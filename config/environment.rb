f5867527f2a881f97ff9323000c5f56a8125df2c
tharon <tharonweighill@gmail.com>
2020-12-05 12:04:15 -0700

    fixed file paths

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require "net/http"
+require "pry"
+require "json"
+require "colorize"
+require "open-uri"
+
+require_relative '../lib/api'
+require_relative '../lib/class'
+require_relative '../lib/cli'
---
+++ lib/api.rb
@@ -0,0 +1,20 @@
+class API
+    
+    def initialize
+        @url = "https://donmallory.tech/rkSpells.json"
+    end 
+
+    def get_spell
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
+
+        all_spells = data("results")
+
+        all_spells.each do |p|
+            Spell.new(p)
+        end 
+    end 
+binding.pry
+
+end 
