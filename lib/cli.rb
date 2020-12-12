5548cf7c50ccf20e241aa5e5b1cfc566e50362dc
tharon <tharonweighill@gmail.com>
2020-12-12 16:33:17 -0700

    able to get spell names by type

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,10 @@
 require_relative "../config/environment.rb"
 
 class CLI
+    attr_accessor  :chosen_type
+
+    
+   
     def run
         API.new
         spell = Spell.all
@@ -37,12 +41,12 @@ class CLI
         
         
 "
-        print_spell_types() 
+       
         
         
         puts "
 Enter a number"
-        enter
+        enter_type
     end 
         
     def print_spell_types
@@ -52,20 +56,38 @@ Enter a number"
     end
         
         
-    def enter
+    def enter_type
         print_spell_types 
         input = gets.chomp()
         if input.to_i >= 7 || input.to_i == 0
             puts "Sorry that's not a spell type!"
-            enter
+            enter_type
         else
             index = (input.to_i) - 1
-            chosen_type = Spell.type[index]
-            puts"                                                                           You have chosen to cast a #{chosen_type}!"   
-       
-            chosen_spell = Spell.name.each.with_index(1) do |name, index|
-                puts "#{index}.#{name}"
-            end
+            @chosen_type = Spell.type[index]
+            puts"                                                                           You have chosen to cast a #{@chosen_type}!"   
+            puts print_spell_names
+        end
+        
+        
+    end     
+
+        def print_spell_names
+            chosen_spells = []
+            index = 1
+            Spell.all.each { |s|
+        if s.type == @chosen_type
+            chosen_spells << "#{index}. #{s.spell}" 
+        index += 1 
         end 
-    end 
+        }    
+
+            chosen_spells      
+       
+        end 
+    
+
+    
 end
+
+# Spell.all.select { |s|s.type = chosen_type }
--- lib/spell.rb
+++ lib/spell.rb
@@ -5,9 +5,7 @@ class Spell
 
     @@all = []
     @@type = []
-    @@name = []
-    @@effect = []
-
+  
   def initialize(hash)
     @@all << self 
       hash.each do |key, value|
@@ -16,21 +14,12 @@ class Spell
     end
  
     @@type << self.type
-    @@name << self
-    @@effect << self.effect
+    
 
   end
 
   def self.search_by_type(type)
     @@all.select { |spell| spell.type == type}
-  end
-
-  def self.search_by_name(name)
-    @@all.select { |spell| spell.name == name}
-  end
-
-  def self.search_by_effect(effect)
-    @@all.select { |spell| spell.effect == effect}
   end
 
   def self.all
@@ -40,14 +29,6 @@ class Spell
   def self.type
     @@type.uniq 
   end
-
-  def self.name
-    @@name
-  end 
-
-  def self.effect
-    @@effect
-  end 
 
 end 
 
